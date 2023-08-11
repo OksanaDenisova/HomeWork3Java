d2d4126c76011df61b5c8f7f59abf900c4a91469
OksanaDenisova <124505469+OksanaDenisova@users.noreply.github.com>
2023-08-11 16:55:48 +0300

    Update UserDataFileWriter.java

--- src/UserDataFileWriter.java
+++ src/UserDataFileWriter.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class UserDataFileWriter {
+import java.io.IOException;
+
+/**
+ * UserDataFileWriter - интерфейс, определяющий контракт для записи данных пользователя. Он содержит
+ * метод writeUserData(), который должен быть реализован в конкретных классах-писателях. Метод может
+ * выбрасывать исключение IOException в случае ошибок ввода-вывода.
+ */
+public interface UserDataFileWriter {
+
+    void writeUserData(UserData userData) throws IOException;
+
 }
