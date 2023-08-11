b9391efd1561d5f81960e0b184506b1c99e91c43
OksanaDenisova <124505469+OksanaDenisova@users.noreply.github.com>
2023-08-11 16:54:59 +0300

    Update ParseException.java

--- src/ParseException.java
+++ src/ParseException.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class ParseException {
+/*ParseException - используется для обработки ошибок, связанных с парсингом данных пользователя.
+ * Конструктор класса принимает сообщение об ошибке и передает его в конструктор базового класса Exception с помощью
+ * ключевого слова super.*/
+
+public class ParseException extends Exception {
+    public ParseException(String message) {
+        super(message);
+    }
 }
