74a78d674647297b8ddc8960f590593ee4fd8d34
OksanaDenisova <124505469+OksanaDenisova@users.noreply.github.com>
2023-08-11 16:54:43 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.IOException;
+import java.util.Scanner;
+
+/*Класс `Main` является точкой входа в программу, где создаются экземпляры классов для чтения
+ * пользовательского ввода и записи данных, а затем вызываются соответствующие методы для обработки
+ * пользовательских данных и записи их в файл.*/
 public class Main {
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        UserInputReader inputReader = new UserInputConsoleReader(scanner);
+        UserDataFileWriter fileWriter = new UserDataFileWriterImpl();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        try {
+            UserData userData = inputReader.readUserData();
+            fileWriter.writeUserData(userData);
+        } catch (UserDataException | IOException e) {
+            System.out.println("Ошибка: " + e.getMessage());
         }
     }
 }
